937625b3cf699cd8110dfa4274a0257d3ab4d405
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-03-29 17:23:23 +0300

    first task - colorSwitcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.start.addEventListener('click', backgroundColorChanger);
+refs.stop.addEventListener('click', stopBackgroundColorChanger);
+
+function backgroundColorChanger() {
+  refs.start.setAttribute('disabled', '');
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopBackgroundColorChanger() {
+  refs.start.removeAttribute('disabled');
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
