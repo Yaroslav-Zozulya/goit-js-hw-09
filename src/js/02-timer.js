a6c652788ab55b314075a4ba7ba45b69139d32a7
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-03-31 14:59:39 +0300

    timer test vertion

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,53 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  datePicker: document.querySelector('#datetime-picker'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  mins: document.querySelector('span[data-minutes]'),
+  secs: document.querySelector('span[data-seconds]'),
+  startBtn: document.querySelector('button[data-start]'),
+};
+let timerTime;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const presentTime = Date.now();
+    const newTime = selectedDates[0];
+    timerTime = newTime - presentTime;
+
+    if (timerTime <= 0) {
+      alert('Please choose a date in the future');
+      return;
+    }
+    const { days, hours, minutes, seconds } = convertMs(timerTime);
+
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = minutes;
+    refs.secs.textContent = seconds;
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+refs.startBtn.addEventListener('click', () => {
+  setInterval(() => {
+    timerTime -= 1000;
+    const { days, hours, minutes, seconds } = convertMs(timerTime);
+
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = minutes;
+    refs.secs.textContent = seconds;
+  }, 1000);
+});
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -20,7 +70,3 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
